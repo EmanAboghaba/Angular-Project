d04ed8068dfcda9c276bc72d96bbe9f1714bfecf
nadaahmed123 <nadaahmed6512@gmail.com>
2025-05-13 00:47:18 +0200

    authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { ExamListComponent } from './components/exam-list.component';
 import { ExamFormComponent } from './components/exam-form.component';
+import { AccountComponent } from './components/account/account.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: ExamListComponent },
   { path: 'add-exam', component: ExamFormComponent },
   { path: 'edit-exam/:id', component: ExamFormComponent },
+   {
+    path: 'account',
+    component: AccountComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
+
 ];
